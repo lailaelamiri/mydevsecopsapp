9c5e65ec26d58830b3d96bcb2bbf998679f83fbb
laila <lailaelamiri586@gmail.com>
2026-04-17 11:52:58 +0100

    fix: flake8 formatting errors

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import subprocess
 
-SECRET_PASSWORD = "admin123"   # ← Bandit will flag this
+
+SECRET_PASSWORD = "admin123"
+
 
 def get_user_input():
     user_input = input("Enter command: ")
-    subprocess.call(user_input, shell=True)  # ← Bandit flags shell injection
+    subprocess.call(user_input, shell=True)
+
 
 def greet(name):
     print(f"Hello, {name}!")
+
 
 if __name__ == "__main__":
     greet("World")
