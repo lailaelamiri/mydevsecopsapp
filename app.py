70a4e1e6db12cb07800a4c5d7e955d2c6b0e640e
laila <lailaelamiri586@gmail.com>
2026-04-18 11:52:45 +0100

    Project 5: DAST with ZAP v3

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"<h1>Hello DevSecOps!</h1>")
 
+
     def log_message(self, format, *args):
         pass  # silence logs
+
 
 if __name__ == "__main__":
     HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
